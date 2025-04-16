cf323f5de0e83236f5f84709364a59744436bff5
Dreamsever <Kimbenijoyce@gmail.com>
2025-04-16 17:20:36 +0100

    Create main.py

---
+++ pokemon-sim/main.py
@@ -0,0 +1,11 @@
+from pokemon import *
+from battle import battle_simulator
+
+
+# Here is where you should add pokémons using their names (always in lower case) and moves (titled names)
+# You can add moves to your pokémons and make them fight against each other with the battle_simulator function
+# Add pokémons using this model:
+# name = pokémons["pokémon"]
+#Add moves using this model:
+# move = moves["Move"]
+# Battle simulator takes two arguments, the first should always be the pokémon that you want to control
